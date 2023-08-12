9833b0ff062dd265dfb8e8b272b38c8e44df750a
Christopher Santos <112826117+ochristopherfilipe@users.noreply.github.com>
2023-08-12 09:41:26 -0400

    gerar analise.py

--- gerar analise.py
+++ gerar analise.py
@@ -23,45 +23,38 @@ def plota_pivot_table(df, value, index, func, ylabel, xlabel, opcao='nada'):
     plt.xlabel(xlabel)
     return None
 
+# mostrando o nome do script
+print ('O nome do nosso script é', sys.argv[0])
 
-# IDEIA PARA AUTOMATIZAR MAIS O PROCESSO:
+# definindo o mes como o argumento do script de 'gerar script'
+mes = sys.argv[1]
+ 
+print('O mês de Referência é', mes)
 
-# Lista de DataFrames dos diferentes meses
-MAR19 = pd.read_csv('SINASC_RO_2019_MAR.csv')
-ABR19 = pd.read_csv('SINASC_RO_2019_ABR.csv')
-MAI19 = pd.read_csv('SINASC_RO_2019_MAI.csv')
-JUN19 = pd.read_csv('SINASC_RO_2019_JUN.csv')
-DEZ19 = pd.read_csv('SINASC_RO_2019_DEZ.csv')
-# Bastaria que colocasse o mês desejado Ex.: 
-# JAN20= pd.read_csv('SINASC_RO_2020_JAN.csv')
+# Definindo sinasc como a base de dados e definindo 'mes' como argumento
+sinasc = pd.read_csv('SINASC_RO_2019_'+mes+'.csv')
 
-meses = [MAR19, ABR19, MAI19, JUN19, DEZ19]  # ,JAN20 ]
+# Criando uma pasta para salvar as figuras de saída com base na data máxima e visualizando o nome da pasta:
+max_data = sinasc.DTNASC.max()[:7]
 
-# Loop cria a pasta  os DataFrames de cada mês
-for mes in meses:
-    max_data = mes.DTNASC.max()[:7]
-    
-    os.makedirs('./output/figs/'+max_data, exist_ok=True)
-    
-# FIM DA IDEIA/
-
+os.makedirs('./output/figs/'+max_data, exist_ok=True)
 
 
 # Plotando e salvando os gráficos desejados com a função 'plota_pivot_table' para cada mes:
-    plota_pivot_table(mes, 'IDADEMAE', 'DTNASC', 'count', 'Quantidade de Nascimentos', 'Data de Nascimento')
+    plota_pivot_table(sinasc, 'IDADEMAE', 'DTNASC', 'count', 'Quantidade de Nascimentos', 'Data de Nascimento')
     plt.savefig('./output/figs/'+max_data+'/quantidade_de_nascimento.png')
 
-    plota_pivot_table(mes, 'IDADEMAE', ['DTNASC', 'SEXO'], 'mean', 'media idade mae','data de nascimento','unstack')
+    plota_pivot_table(sinasc, 'IDADEMAE', ['DTNASC', 'SEXO'], 'mean', 'media idade mae','data de nascimento','unstack')
     plt.savefig('./output/figs/'+max_data+'/media idade mae por sexo.png')
 
-    plota_pivot_table(mes, 'PESO', ['DTNASC', 'SEXO'], 'mean', 'media peso bebe','data de nascimento','unstack')
+    plota_pivot_table(sinasc, 'PESO', ['DTNASC', 'SEXO'], 'mean', 'media peso bebe','data de nascimento','unstack')
     plt.savefig('./output/figs/'+max_data+'/media peso bebe por sexo.png')
 
-    plota_pivot_table(mes, 'PESO', 'ESCMAE', 'median', 'apgar1 medio','gestacao','sort')
+    plota_pivot_table(sinasc, 'PESO', 'ESCMAE', 'median', 'apgar1 medio','gestacao','sort')
     plt.savefig('./output/figs/'+max_data+'/media apgar1 por escolaridade mae.png')
 
-    plota_pivot_table(mes, 'APGAR1', 'GESTACAO', 'mean', 'apgar1 medio','gestacao','sort')
+    plota_pivot_table(sinasc, 'APGAR1', 'GESTACAO', 'mean', 'apgar1 medio','gestacao','sort')
     plt.savefig('./output/figs/'+max_data+'/media apgar1 por gestacao.png')
 
-    plota_pivot_table(mes, 'APGAR5', 'GESTACAO', 'mean', 'apgar5 medio','gestacao','sort')
+    plota_pivot_table(sinasc, 'APGAR5', 'GESTACAO', 'mean', 'apgar5 medio','gestacao','sort')
     plt.savefig('./output/figs/'+max_data+'/media apgar5 por gestacao.png')
